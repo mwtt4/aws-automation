cdc7d624a7139eeb15888a8cc0c5ba4ed5827fe5
mwtt4 <39355558+mwtt4@users.noreply.github.com>
2023-11-09 11:41:03 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -20,7 +20,7 @@ print(r"""{}
                 ▄▀▒▒▒▒▒▄██▀▄▄░░▀▄▄▀░░▄▄▀█▄░█▀▒▒▒▒
                                                
                                                                                                                                                         
-                {}{}☁️{}   CSIRT Cloud Automation 
+                    {}{}☁️{}   CSIRT Cloud Automation 
                                         
     """.format(Fore.LIGHTBLACK_EX, Fore.BLUE,
                Fore.LIGHTBLACK_EX, Fore.WHITE))
@@ -80,11 +80,11 @@ elif args.command == 'get-tags-s3':
 ################################## EC2 ##################################
 
 elif args.command == 'list-ec2':
-    ec2 = boto3.client('ec2', region_name='us-east-1')
-    
+    ec2 = boto3.client('ec2', region_name='sa-east-1')
+
     # Lista as instâncias EC2
     response = ec2.describe_instances()
-    
+
     print("EC2 Instances:")
     for reservation in response['Reservations']:
         for instance in reservation['Instances']:
@@ -94,18 +94,20 @@ elif args.command == 'list-ec2':
             for interface in instance['NetworkInterfaces']:
                 print(f"Private IP: {interface['PrivateIpAddress']}")
                 print(f"Public IP: {interface.get('Association', {}).get('PublicIp', 'N/A')}")
+                print(f"Elastic IP: {interface.get('Association', {}).get('PublicIp', 'N/A')}")
+            print(f"Launch Date: {instance['LaunchTime']}")
             print()
 
 elif args.command == 'get-tags-ec2':
     if not args.instance_id:
         print("Error: You must provide the --instance-id argument for 'get-tags-ec2' command.")
     else:
-        ec2 = boto3.client('ec2', region_name='us-east-1')
-        
+        ec2 = boto3.client('ec2', region_name='sa-east-1')
+
         # Obtém as tags da instância EC2 especificada
         try:
             response = ec2.describe_tags(Filters=[{'Name': 'resource-id', 'Values': [args.instance_id]}])
-            
+
             print(f"Tags for EC2 Instance ID '{args.instance_id}':")
             for tag in response['Tags']:
                 print(f"- {tag['Key']}: {tag['Value']}")
